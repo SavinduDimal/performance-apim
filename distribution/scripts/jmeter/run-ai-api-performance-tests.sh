6aaa13ebfd54811f43e0d81c0a2583f8acc3531c
SavinduDimal <dimalsavindu@gmail.com>
2026-04-24 14:57:02 +0530

    Add temp logs to find the git version

--- distribution/scripts/jmeter/run-ai-api-performance-tests.sh
+++ distribution/scripts/jmeter/run-ai-api-performance-tests.sh
@@ -9,6 +9,9 @@ script_dir=$(dirname "$0")
 export PAYLOAD_GENERATOR_ARGS="-a"
 jwt_tokens_file_name="jwt-tokens.csv"
 jwt_tokens_count=4000
+
+echo "#### Current git change version: performance-apim: v1 ####"
+
 . $script_dir/perf-test-common.sh
 
 function initialize() {
